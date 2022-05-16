1987b0f7833b74c072e00b9f25aa4ea63f096079
Red Riot <itxpapon@gmail.com>
2022-05-16 14:38:10 -0500

    Changes commited for java getter and setter

---
+++ android/src/main/java/GetSet.java
@@ -0,0 +1,27 @@
+public class GetSet {
+    int id;
+    String name;
+
+    public GetSet(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+
+    }
+    public String getsName()
+    {
+        return name;
+    }
+
+}
