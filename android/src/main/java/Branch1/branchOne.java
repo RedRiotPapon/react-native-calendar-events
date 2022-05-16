c191989e21009cfd62c8685508dbf598d025ff72
Red Riot <itxpapon@gmail.com>
2022-05-16 15:40:03 -0500

    branch2 modified

--- android/src/main/java/Branch1/branchOne.java
+++ android/src/main/java/Branch1/branchOne.java
@@ -3,11 +3,23 @@ package Branch1;
 public class branchOne {
     int id;
     String Address;
+    int s_Class;
 
-    public branchOne(int id, String address) {
+    public branchOne(int id, String address, int s_Class) {
         this.id = id;
         Address = address;
+        this.s_Class = s_Class;
     }
+
+    public void setS_Class(int s_Class) {
+        this.s_Class = s_Class;
+    }
+
+    public int getS_Class() {
+        return s_Class;
+    }
+
+
 
     public int getId() {
         return id;
